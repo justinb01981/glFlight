37bc4afbc426401ae3e29c99f09d3a24f1198eaf
Justin Brady <justin@domain17.net>
2018-11-25 08:52:54 -0800

    bind to 10 sockets at a time

--- glflight_server.py
+++ glflight_server.py
@@ -3,6 +3,8 @@
 import socket
 import datetime
 import struct
+from multiprocessing import Process
+from time import sleep
 
 ## gamemessage
     #    unsigned int vers;
@@ -50,45 +52,72 @@ def gametime():
     return datetime.datetime.now()
 
 
-## main
+## server process
 
-UDP_IP = "0.0.0.0"
-UDP_PORT = 52000
+def socketServer(sock, port, done):
 
-sock = socket.socket(socket.AF_INET, # Internet
-                     socket.SOCK_DGRAM) # UDP
+    UDP_IP = "0.0.0.0"
+    UDP_PORT = port
+    sock.bind((UDP_IP, UDP_PORT))
+    print("bound to "+str(UDP_PORT))
 
-sock.bind((UDP_IP, UDP_PORT))
+    all_players = []
+    player_ignored = GamePlayer(sock, ("0.0.0.0", 0))
 
-all_players = []
-player_ignored = GamePlayer(sock, ("0.0.0.0", 0))
+    while not done:
 
-while 1:
-
-    data, addr = sock.recvfrom(GameMessage.MAX_LEN)
-    msg = GameMessage(data)
-    
-    player = None    
-    for p in all_players:
-        if p.address == addr:
-            player = p
-        if msg.player_id == p.player_id:
-            ## id collision
-            player = player_ignored
-    if player == None:
-        player = GamePlayer(sock, addr)
-        player.player_id = msg.player_id
-        all_players.append(player)
-        print("added player" + str(player.address))
-    
-    for p in all_players:
-        if p.address != addr:
-            if gametime() - p.time_last_active > datetime.timedelta(seconds=60):
-                all_players.remove(p)
-                print("timed out player" + str(p.address))
+        data, addr = sock.recvfrom(GameMessage.MAX_LEN)
+        msg = GameMessage(data)
+        
+        player = None    
+        for p in all_players:
+            if p.address == addr:
+                player = p
+            if msg.player_id == p.player_id:
+                ## id collision
+                player = player_ignored
+        if player == None:
+            player = GamePlayer(sock, addr)
+            player.player_id = msg.player_id
+            all_players.append(player)
+            #print("added player" + str(player.address))
+        
+        for p in all_players:
+            if p.address != addr:
+                if gametime() - p.time_last_active > datetime.timedelta(seconds=60):
+                    all_players.remove(p)
+                    #print("timed out player" + str(p.address))
+                else:
+                    p.send(msg.data)
             else:
-                p.send(msg.data)
-        else:
-            p.time_last_active = gametime()
+                p.time_last_active = gametime()
+
+    print("exiting")
+
+if __name__ == '__main__':
+    UDP_PORT = 52000
+    children = []
+    sockets = []
+    exiting = 0
+    while len(children) < 10:
+
+        sock = socket.socket(socket.AF_INET, # Internet
+                             socket.SOCK_DGRAM) # UDP
+        sockets += [sock]
+
+        child = Process(target=(socketServer), args=(sock, UDP_PORT+len(children), exiting))
+        children += [child]
+        child.start()
 
 
+    sleep(5)
+
+    ## TODO: child processes will be stuck in read() - must close socket from parent?
+    exiting = 1
+    n = 0
+    for s in sockets:
+        s.sendto(bytes("die", "UTF-8"), ("127.0.0.1", UDP_PORT+n))
+        n += 1
+
+    for child in children:
+        child.join()
