c5b36bb8b5e5cccb38a6ecd28cc646640397144c
Justin Brady <justinb01981@bitbucket.org>
2024-01-30 07:40:58 -0800

    version bump

--- android/gradle-build/glFlightImport/app/build/generated/source/buildConfig/debug/com/domain17/glflight/BuildConfig.java
+++ android/gradle-build/glFlightImport/app/build/generated/source/buildConfig/debug/com/domain17/glflight/BuildConfig.java
@@ -7,6 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
   public static final String APPLICATION_ID = "com.domain17.glflight";
   public static final String BUILD_TYPE = "debug";
-  public static final int VERSION_CODE = 1100305;
-  public static final String VERSION_NAME = "1.10.3.6";
+  public static final int VERSION_CODE = 11004;
+  public static final String VERSION_NAME = "1.10.4";
 }
