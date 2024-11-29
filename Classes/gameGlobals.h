9b1b86d76f4b30dcec6a8e45a11407323ecbaf98
Justin Brady <justin@domain17.net>
2024-11-29 09:35:08 -0800

    launchscreen rotation fix removed from plist defaults to default.png for launch screen yay bump version

--- Classes/gameGlobals.h
+++ Classes/gameGlobals.h
@@ -98,7 +98,7 @@ extern void (*glFlightOnPurchase)(void);
 /* reducing friction influences this... should be MAX_THRUST really */
 #define MAX_SPEED (8.25)
 
-const static char* GAME_VERSION_STR = "1.10.7_core";
+const static char* GAME_VERSION_STR = "1.11.0_core";
 
 const static char* GAME_NETWORK_DIRECTORY_HOSTNAME_DEFAULT = "d0gf1ght.domain17.net";
 
