4e8111658b8cb364a6af67d9bbc980daaea33b4c
Justin Brady <justin@domain17.net>
2025-10-24 22:06:39 -0700

    bounding texture formulas less fucked

--- Classes/world.h
+++ Classes/world.h
@@ -24,7 +24,7 @@
 //#define WORLD_MAX_REGIONS /*50*/ 25
 #define WORLD_MAX_PLANES 16
 #define WORLD_MAX_TRIANGLE_MESH 16
-#define WORLD_BOUNDING_SPHERE_STEPS ((float) 8)
+#define WORLD_BOUNDING_SPHERE_STEPS (8)
 #define WORLD_TERRAIN_COMPLEXITY 64
 
 // a little fudgey - see world.c
